6fe024a5b25ffc1b0e091995f9180df72aa3d365
Sergey Fedorov <Fedorov.Sergey@tatar.ru>
2016-08-04 14:09:24 +0300

    add hook

--- webhook.php
+++ webhook.php
@@ -1,7 +1,14 @@
 <?php
-print_r('Welcome to televk');
 
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-die();
+$loader = require __DIR__.'/vendor/autoload.php';
+
+$API_KEY = '197232756:AAG-0ZBcvYPU_xVdyptrRXFo5HfJajvNfuQ';
+$BOT_NAME = 'tele2vk_bot';
+
+try {
+    $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
+
+    $telegram->handle();
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // echo $e->getMessage();
+}
