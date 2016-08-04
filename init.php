498251aaa01168ebb6eaa7a45f6bbb65dad1810c
Sergey Fedorov <Fedorov.Sergey@tatar.ru>
2016-08-04 14:20:25 +0300

    del vendor

--- init.php
+++ init.php
@@ -1,14 +1,19 @@
 <?php
-
-$loader = require __DIR__.'/vendor/autoload.php';
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
 
 $API_KEY = '197232756:AAG-0ZBcvYPU_xVdyptrRXFo5HfJajvNfuQ';
 $BOT_NAME = 'tele2vk_bot';
-
+$hook_url = 'https://televk.herokuapp.com/webhook.php';
 try {
+    // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
 
-    echo $telegram->setWebHook('https://televk.herokuapp.com/webhook.php');
+    // Set webhook
+    $result = $telegram->setWebHook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    echo $e->getMessage();
+    echo $e;
 }
